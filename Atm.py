b8d07a8689cc5654d985781c9e04dbdc4e59e5fa
hiteshpundhir25 <70944346+hiteshpundhir25@users.noreply.github.com>
2022-01-28 19:33:03 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,25 @@
+class Atm(object):
+    def __init__(self, name, age, card_number, pin_number)
+        self.name = name
+        self.age = age
+        self.card_number = card_number
+        self.pin_number = pin_number
+    
+    def start(self):
+        print("started")
+
+    def stop(self):
+        print("stopped")
+
+    def cashWithdrawl(self):
+        print("The cash is withdrawing...")
+        "withdrawl functionality here."
+
+    def cashDeposit(self):
+        print("The cash is depositing...")
+        "deposit functionality here."
+
+    def check_balance(self):
+        print("Checking the balance...")
+        "balance functionality here."
+        
